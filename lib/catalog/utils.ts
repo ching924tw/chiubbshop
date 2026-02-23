d47206a72b1299147100dcff2b05e948caf639c4
ching924tw <ching924tw@gmail.com>
2026-02-23 23:27:40 +0800

    Add product retrieval functions to utils

--- lib/catalog/utils.ts
+++ lib/catalog/utils.ts
@@ -1,1 +1,13 @@
+import { products } from "./data";
+import { Product } from "./types";
 
+export function getAllProducts(): Product[] {
+  return products
+    .filter((p) => p.published)
+    .slice()
+    .sort((a, b) => (a.createdAt < b.createdAt ? 1 : -1));
+}
+
+export function getProductBySlug(slug: string): Product | undefined {
+  return products.find((p) => p.slug === slug && p.published);
+}
